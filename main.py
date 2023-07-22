a2eb1aaf40cfe0cbd322666cef260833e5d826e2
mathusanmanoharan <mathusan54@gmail.com>
2023-07-22 10:20:48 -0400

    Completed Program ; Sends the difference in price between yesturday and before yeturday with the news associated with it by relevance based on if it percentage is higher then 5%

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import requests
 import datetime
+import smtplib
 
-yesturday_date = datetime.datetime.now() - datetime.timedelta(days=1)
-yesturday_date = str(yesturday_date).split(" ")
-yesturday_date = yesturday_date[0]
+#All the variables and constants
+MY_EMAIL="Mathudeals@gmail.com"
+MY_PASS="ngzjeqhpuqfwbqja"
 
-before_yesturday_date = datetime.datetime.now() - datetime.timedelta(days=2)
-before_yesturday_date = str(before_yesturday_date).split(" ")
-before_yesturday_date = before_yesturday_date[0]
-
-STOCK_NAME = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+STOCK_NAME = "AAPL"
+COMPANY_NAME = "Apple Inc"
 
 MY_API_NEWS = "5d12d240957247eeb7c1f0055528a9c1"
 MY_API_STOCKS = "PU5549K5KHK695KN"
@@ -23,27 +20,28 @@ params = {
     "apikey": MY_API_STOCKS
 }
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+#The date from yesturday and the day before yesturday
+yesturday_date = (str(datetime.datetime.now() - datetime.timedelta(days=1)).split(" "))[0]
+before_yesturday_date = (str(datetime.datetime.now() - datetime.timedelta(days=2)).split(" "))[0]
+
+#Request to API Stock for all the values daily
 response_stock = requests.get(url="https://www.alphavantage.co/query", params=params)
 dic_time = response_stock.json()["Time Series (Daily)"]
 
+#Make a list for the time
 list_time = [value for (key,value) in dic_time.items()]
+
+#Choose the closing price for yesturday and the before yesturday
 yesturday = float(list_time[0]["4. close"])
 before_yesturday =  float(list_time[1]["4. close"])
 
 ##Difference in price
-difference = abs(before_yesturday-yesturday)
-print(difference)
+difference = abs(yesturday-before_yesturday)
+
 #Percentage difference
-print(yesturday)
-print(before_yesturday)
 percentage = round((difference / yesturday) * 100,1)
-print(percentage)
-print(dic_time)
-print(yesturday_date)
-print(before_yesturday_date)
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+
+#SendEmail if difference is above 5%
 if percentage > 0:
     print("Get News")
     params = {
@@ -51,35 +49,42 @@ if percentage > 0:
         "from": yesturday_date,
         "sortBy": "publishedAt",
         "apikey": MY_API_NEWS,
-
     }
+    #Resquest to API News to get all top news
     response_news = requests.get(url = "https://newsapi.org/v2/everything",params=params)
-    ## STEP 2: https://newsapi.org/
     news_dic = response_news.json()
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+    #Keep the top 3 news in a list of dictionaries of title and description
     list_news = [value for (key,value) in news_dic.items()]
     top_news = []
     for _ in range(3):
         top_news.append(list_news[2][_])
         print(top_news[_])
+    three_news =  [{'title': dictionary['title'], 'description': dictionary['description']} for dictionary in top_news[:3]]
+    print(three_news)
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
+    # See if the difference is positive or negative
+    emo = " "
+    if (yesturday - before_yesturday) >0:
+        emo = f"🔺{percentage}% "
+    else:
+        emo = f"🔻{percentage}% "
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+    #Sends all 3 articles Seperately
+    for article in three_news:
+        #Prepare message to send by email
+        msg =f"Subject: {COMPANY_NAME} Stock Update\n\n {STOCK_NAME} :{emo}\nHeadline: {article['title']}\nBrief: {article['description']}"
+        msg = msg.encode("utf-8")
 
-#TODO 9. - Send each article as a separate message via Twilio. 
+        #Send email to User
+        with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL,MY_PASS)
+            connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=msg
+        )
 
 
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
